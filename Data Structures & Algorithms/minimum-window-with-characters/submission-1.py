6ea400c3816b2ada275cb4571411a5ba1959b3fd
Raza <108403182+owaisraza1704@users.noreply.github.com>
2026-07-02 22:29:49 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        s1_hash = {}
+        for i in range(len(s)):
+            s1_hash[s[i]] = i
+        
+        indexes = []
+        
+        for string in t:
+            if string not in s1_hash:
+                return ""
+            indexes.append(s1_hash[string])
+        
+        indexes.sort()
+        minIdx = indexes[0]
+        maxIdx = indexes[-1]
+        return s[minIdx:maxIdx+1]
